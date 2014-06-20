8e5265bfbc56175d80d316f59044b1e5ffef55f9
Daniel Landau <dlandau@reed.edu>
2014-06-20 10:00:44 -0700

    add current_user handling

--- app/controllers/backbone_controller.rb
+++ app/controllers/backbone_controller.rb
@@ -1,6 +1,14 @@
 class BackboneController < ApplicationController
   layout 'backbone'
 
+  before_filter :set_current_user
+
+  def set_current_user
+    @user = User.first
+
+    gon.rabl "app/views/users/show.json.rabl", as: "current_user"
+  end
+
   def lesson
     @lesson = Lesson.find(params[:id])
 
