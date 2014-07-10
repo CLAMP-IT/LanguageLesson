ab9094bc6c21ca1626605503cf3de7ad99d79248
Daniel Landau <nprolog@postalmodern.com>
2014-07-10 11:10:03 -0400

    change recording association

--- app/models/question_attempt_response.rb
+++ app/models/question_attempt_response.rb
@@ -1,5 +1,5 @@
 class QuestionAttemptResponse < ActiveRecord::Base
   belongs_to :question_attempt
   belongs_to :user_id
-  has_one :response_recording, as: :recordable
+  has_one :recording, as: :recordable, dependent: :destroy
 end
