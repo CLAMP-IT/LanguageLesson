f3066ffcf0d01c08ea139fff592d88070f5099be
Daniel Landau <nprolog@postalmodern.com>
2014-07-15 14:13:56 -0400

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701220625) do
+ActiveRecord::Schema.define(version: 20140714213413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 20140701220625) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "institutions", force: true do |t|
+    t.string   "name"
+    t.string   "identifier"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "lesson_attempts", force: true do |t|
