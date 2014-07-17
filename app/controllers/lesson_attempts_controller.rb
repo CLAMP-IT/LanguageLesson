39432ee3d8c8a2b7468490c17b2413efb7104861
Daniel Landau <dlandau@reed.edu>
2014-07-16 20:33:39 -0700

    add quesstion_attempt ordering by question

--- app/controllers/lesson_attempts_controller.rb
+++ app/controllers/lesson_attempts_controller.rb
@@ -13,7 +13,7 @@ class LessonAttemptsController < ApplicationController
   # GET /lessons/1
   # GET /lessons/1.json
   def show
-    @lesson_attempt = LessonAttempt.find(params[:id])
+    @lesson_attempt = LessonAttempt.includes(:question_attempts => :question).order('questions.row_order').find(params[:id])
 
     respond_to do |format|
       format.html
