16c697bc15f7a6d1aa3215ea1edd6d72853f6abb
Daniel Landau <nprolog@postalmodern.com>
2014-07-17 19:56:46 -0700

    add associations

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,6 +1,8 @@
 class ContentBlock < ActiveRecord::Base
   has_one :page_element, as: :pageable
+  has_one :lesson_element, as: :presentable
   #attr_accessible :page_element, :title, :content
+  has_one :recording, as: :recordable
 
   def type
     "ContentBlock"
