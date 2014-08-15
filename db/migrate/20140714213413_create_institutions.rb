f88fbfe94716f5415bcccd46913241ed874ffa2b
Daniel Landau <dlandau@reed.edu>
2014-08-15 09:34:02 -0700

    added request flags

--- db/migrate/20140714213413_create_institutions.rb
+++ db/migrate/20140714213413_create_institutions.rb
@@ -3,11 +3,14 @@ class CreateInstitutions < ActiveRecord::Migration
     create_table :institutions do |t|
       t.string :name
       t.string :identifier
+      t.boolean :active
+      t.boolean :requested, default: false
+      t.boolean :accepted, default: false
 
       t.timestamps
     end
 
-    ['Reed College', 'Carleton College'].each { |iname| Institution.create(name: iname, identifier: SecureRandom.hex) } 
+    ['Reed College', 'Carleton College'].each { |iname| Institution.create(name: iname, identifier: SecureRandom.hex, active: true) } 
   end
 
   def down
