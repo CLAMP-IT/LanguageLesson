3790b42d70de3f117bd98ab6fbf9126f85574397
Daniel Landau <nprolog@postalmodern.com>
2014-08-15 09:55:01 -0700

    add user reference

--- db/migrate/20130612200938_create_question_attempts.rb
+++ db/migrate/20130612200938_create_question_attempts.rb
@@ -3,10 +3,12 @@ class CreateQuestionAttempts < ActiveRecord::Migration
     create_table :question_attempts do |t|
       t.references :lesson_attempt
       t.references :question
+      t.references :user
 
       t.timestamps
     end
     add_index :question_attempts, :lesson_attempt_id
     add_index :question_attempts, :question_id
+    add_index :question_attempts, :user_id
   end
 end
