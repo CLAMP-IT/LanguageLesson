8f6f8f85107b0f89ba23d41e7c4448460441d568
Daniel Landau <nprolog@postalmodern.com>
2015-05-17 17:57:04 -0400

    add initial Activity model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ActiveRecord::Base
   #attr_accessible :moodle_id, :name
   has_many :course_lessons
-  has_many :lessons, through: :course_lessons 
+  has_many :lessons, through: :course_lessons
+  has_many :activities
 end
