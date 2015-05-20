e0cf01e43694858841d3ea83b08472865728daac
Daniel Landau <nprolog@postalmodern.com>
2015-05-20 09:33:59 -0400

    add validators

--- app/models/lesson_attempt.rb
+++ app/models/lesson_attempt.rb
@@ -3,4 +3,6 @@ class LessonAttempt < ActiveRecord::Base
   belongs_to :lesson
   #attr_accessible :user, :lesson
   has_many :question_attempts, dependent: :destroy
+
+  validates_presence_of :user_id, :lesson_id
 end
