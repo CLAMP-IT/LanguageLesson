8a1748356e48d70970a94923a811fb60ea4fd84f
Daniel Landau <dlandau@reed.edu>
2015-05-24 18:12:33 -0700

    update recording handling

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,8 +1,7 @@
 class ContentBlock < ActiveRecord::Base
   has_one :page_element, as: :pageable
   has_one :lesson_element, as: :presentable
-  #attr_accessible :page_element, :title, :content
-  has_one :recording, as: :recordable
+  has_one :recording, -> { where(recording_type: Recording::RECORDING_TYPE_STANDARD) }, as: :recordable, dependent: :destroy
 
   def type
     "ContentBlock"
