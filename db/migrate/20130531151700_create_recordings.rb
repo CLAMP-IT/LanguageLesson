022218bfc2ad0b6090029fdf87d43b33f1af9314
Daniel Landau <nprolog@postalmodern.com>
2015-09-04 10:42:55 -0700

    add file fields to recordings

--- db/migrate/20130531151700_create_recordings.rb
+++ db/migrate/20130531151700_create_recordings.rb
@@ -1,7 +1,12 @@
 class CreateRecordings < ActiveRecord::Migration
   def change
     create_table :recordings do |t|
-      t.attachment :file
+      t.string :url
+      t.string :bucket_name, limit: 40
+      t.string :uuid
+      t.string :file_name
+      t.integer :file_size
+      t.string :content_type
       t.references :recordable, :polymorphic => true
       t.string :recording_type, limit: 30
       t.timestamps
