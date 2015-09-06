eeb2283babf324372f8c718326f68e4f085fd512
Daniel Landau <dlandau@reed.edu>
2015-09-06 16:01:11 -0700

    remove comment

--- app/models/lesson_attempt.rb
+++ app/models/lesson_attempt.rb
@@ -1,7 +1,6 @@
 class LessonAttempt < ActiveRecord::Base
   belongs_to :user
   belongs_to :lesson
-  #attr_accessible :user, :lesson
   has_many :question_attempts, dependent: :destroy
 
   validates_presence_of :user_id, :lesson_id
