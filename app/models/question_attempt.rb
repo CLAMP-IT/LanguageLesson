b17bd281c3dd86c3732b5124b2f85c11fee80a12
Daniel Landau <dlandau@reed.edu>
2015-09-14 20:53:41 -0700

    change association to has_one

--- app/models/question_attempt.rb
+++ app/models/question_attempt.rb
@@ -2,6 +2,8 @@ class QuestionAttempt < ActiveRecord::Base
   belongs_to :lesson_attempt
   belongs_to :question
   belongs_to :user
-  has_many :recordings, as: :recordable
+  has_one :recording, as: :recordable, dependent: :destroy
+  accepts_nested_attributes_for :recording
+
   has_many :question_attempt_responses, dependent: :destroy
 end
