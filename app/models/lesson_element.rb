b9620c8acb769cf433fee83fe3658139c8b01d4f
Daniel Landau <dlandau@reed.edu>
2015-09-19 08:02:43 -0700

    add first? and last? methods

--- app/models/lesson_element.rb
+++ app/models/lesson_element.rb
@@ -4,4 +4,12 @@ class LessonElement < ActiveRecord::Base
   belongs_to :lesson
   belongs_to :presentable, polymorphic: true
   has_one :course, through: :lesson
+
+  def first?
+    lesson.lesson_elements.index(self) == 0
+  end
+
+  def last?
+    lesson.lesson_elements.index(self) == (lesson.lesson_elements.size - 1)
+  end
 end
