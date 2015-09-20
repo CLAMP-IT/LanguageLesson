45f43dc94883fb575910b8273654fa5a90fdc0da
Daniel Landau <dlandau@reed.edu>
2015-09-19 19:34:34 -0700

    remove unused associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,5 @@
 class Question < ActiveRecord::Base
   include RankedModel
   ranks :row_order
-  has_one :lesson, through: :page
-  has_many :answers
-  #has_many :question_recordings
-  #has_many :recordings, through: :question_recordings
   has_many :recordings, as: :recordable
-  #attr_accessible :title, :content, :lesson, :row_order_position, :page_element, :recordable
 end
