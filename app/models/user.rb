ca1f8cebb0814050c490186beff883168243d44b
Daniel Landau <dlandau@reed.edu>
2015-09-20 21:49:46 -0700

    add question_attempts association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   #attr_accessible :email, :moodle_id, :name
   validates_presence_of :email, :name
 
+  has_many :question_attempts
+  
   belongs_to :institution
 end
