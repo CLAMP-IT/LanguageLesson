f9be9ff9abe63d400b9aebf8500d657ff0c0f729
Daniel Landau <dlandau@reed.edu>
2015-09-20 21:45:31 -0700

    add associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,6 @@ class Question < ActiveRecord::Base
   ranks :row_order
   has_many :recordings, as: :recordable
   has_many :question_attempts
+  has_many :lesson_elements, as: :presentable
+  has_many :lessons, through: :lesson_elements
 end
