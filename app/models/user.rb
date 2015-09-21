ba6bfee188d49c5c8024032b4bac327954b6ad80
Daniel Landau <dlandau@reed.edu>
2015-09-20 21:50:08 -0700

    use for_activity scope to filter question_attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   has_many :question_attempts
   
   belongs_to :institution
+
+  def self.with_question_attempts_for_activity(activity)
+    joins(:question_attempts).merge( QuestionAttempt.for_activity(activity) )
+  end
 end
