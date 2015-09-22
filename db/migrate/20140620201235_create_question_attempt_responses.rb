24dcf2884524e230d9a2769a070c5d43e0111555
Daniel Landau <dlandau@reed.edu>
2015-09-22 05:57:09 -0700

    fix migration

--- db/migrate/20140620201235_create_question_attempt_responses.rb
+++ db/migrate/20140620201235_create_question_attempt_responses.rb
@@ -2,7 +2,7 @@ class CreateQuestionAttemptResponses < ActiveRecord::Migration
   def change
     create_table :question_attempt_responses do |t|
       t.references :question_attempt, index: true
-      t.references :user_id, index: true
+      t.references :user, index: true
       t.text :note
       t.string :mark_start
       t.string :mark_end
